b7bb6675e40da1dce0901af2106da2e4d228fa78
Dheeraj kumar <dheerajgupta0651@gmail.com>
2024-01-06 18:19:22 +0530

    test line added in app

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-
-import './App.css';
-import Navbaar from './components/header/Navbaar';
-import Newnav from './components/newnaav/Newnav';
-import Maicomp from './components/home/Maicomp';
-import Footer from './components/footer/Footer';
-import Sign_in from './components/signup_sign/Sign_in';
-import SignUp from './components/signup_sign/SignUp';
-import Cart from './components/cart/Cart';
-import Buynow from './components/buynow/Buynow';
-import {Routes,Route} from "react-router-dom";
-
+import "./App.css";
+import Navbaar from "./components/header/Navbaar";
+import Newnav from "./components/newnaav/Newnav";
+import Maicomp from "./components/home/Maicomp";
+import Footer from "./components/footer/Footer";
+import Sign_in from "./components/signup_sign/Sign_in";
+import SignUp from "./components/signup_sign/SignUp";
+import Cart from "./components/cart/Cart";
+import Buynow from "./components/buynow/Buynow";
+import { Routes, Route } from "react-router-dom";
+console.log("test");
 function App() {
   return (
     <>
-   <Navbaar/>
-   <Newnav/>
-   <Routes>
-    <Route path='/' element={<Maicomp/>}/>
-    <Route path='/login' element={<Sign_in/>}/>
-    <Route path='/signup' element={<SignUp/>}/>
-    <Route path='/getproductsone/:id' element={<Cart/>}/>
-    <Route path='/buynow' element={<Buynow/>}/>
-   </Routes>
-   
-    <Footer/>
+      <Navbaar />
+      <Newnav />
+      <Routes>
+        <Route path="/" element={<Maicomp />} />
+        <Route path="/login" element={<Sign_in />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/getproductsone/:id" element={<Cart />} />
+        <Route path="/buynow" element={<Buynow />} />
+      </Routes>
+
+      <Footer />
     </>
   );
 }
